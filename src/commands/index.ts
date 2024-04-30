0c1654157284dfe30b6058433664ef1e8c129288
PMtHk <zooby88@naver.com>
2024-04-30 18:26:10 +0900

    feat : /도움말 명령어 추가

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,10 @@
 import { deregister } from './deregister'
+import { guide } from './guide'
 import { ping } from './ping'
 import { register } from './register'
 import { search } from './search'
 import { watchList } from './watchList'
 
-const commands = [ping, search, watchList, register, deregister]
+const commands = [ping, search, watchList, register, deregister, guide]
 
 export default commands
