cf601f4a0f8c70102e9898d3c6aea0ffc3c18bef
PMtHk <zooby88@gmail.com>
2024-11-18 16:24:32 +0900

    refactor : server.ts 리팩토링
    - 매직 넘버를 상수로 변경
    - createServer 와 startServer 함수 분리
    - 불필요한 주석 제거

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,64 @@
 import 'reflect-metadata'
-
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import { initBot } from './bot'
 
-const app = express()
+const PORT = parseInt(process.env.PORT) || 8000
 
-app.get('/health', (req, res) => {
-  res.send({
-    status: 'ok',
-    timestamp: new Date().toLocaleString('ko-KR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      timeZoneName: 'short',
-    }),
+const STATUS = {
+  OK: 200,
+  INTERNAL_SERVER_ERROR: 500,
+} as const
+
+const MESSAGE = {
+  OK: '서버가 정상적으로 작동 중입니다.',
+  INTERNAL_SERVER_ERROR: '서버에 문제가 발생했습니다.',
+} as const
+
+const TIMESTAMP_OPTIONS: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZoneName: 'short',
+}
+
+function createServer() {
+  const app = express()
+
+  app.get('/health', (req, res) => {
+    const response = {
+      status: STATUS.OK,
+      message: MESSAGE.OK,
+      timestamp: new Date().toLocaleString('ko-KR', { ...TIMESTAMP_OPTIONS }),
+    }
+
+    res.status(200).json(response)
   })
-})
 
-app.listen(8000, () => {
-  console.log('GoroseyBot Server is running on port 8000')
-})
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack)
 
-initBot()
+    res.status(500).send({
+      status: STATUS.INTERNAL_SERVER_ERROR,
+      message: MESSAGE.INTERNAL_SERVER_ERROR,
+    })
+  })
+
+  return app
+}
+
+function startServer(port: number = PORT) {
+  const app = createServer()
+
+  try {
+    app.listen(port, () => {
+      initBot()
+    })
+  } catch (e) {
+    console.error('서버를 시작하는 중에 문제가 발생했습니다.')
+  }
+}
+
+startServer()
