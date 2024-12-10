3eede9158984b5f087a085ae3df9f954edd8b4bc
PMtHk <zooby88@gmail.com>
2024-12-10 18:14:02 +0900

    refactor: KoreanBots 의 싱글톤 인스턴스 export 추가

--- src/libs/koreanbots/KoreanBots.ts
+++ src/libs/koreanbots/KoreanBots.ts
@@ -1,8 +1,18 @@
 import { HttpClient } from '../http'
-import { UpdateBotStatResponse } from './types'
+import { UpdateBotStatDto, UpdateBotStatResponse } from './types'
 import { KoreanBotsError } from './error'
 
-export class KoreanBotsClient {
+const { DISCORD_APPLICATION_ID, KOREAN_DISCORD_LIST_TOKEN } = process.env
+
+if (!DISCORD_APPLICATION_ID) {
+  throw new Error('DISCORD_APPLICATION_ID를 불러올 수 없습니다.')
+}
+
+if (!KOREAN_DISCORD_LIST_TOKEN) {
+  throw new Error('KOREAN_DISCORD_LIST_TOKEN을 불러올 수 없습니다.')
+}
+
+export class KoreanBots {
   private http: HttpClient
 
   constructor(
@@ -19,9 +29,7 @@ export class KoreanBotsClient {
     })
   }
 
-  async updateStats(
-    stats: UpdateBotStatResponse,
-  ): Promise<UpdateBotStatResponse> {
+  async updateStats(stats: UpdateBotStatDto): Promise<UpdateBotStatResponse> {
     const response = await this.http.post<UpdateBotStatResponse>(
       `${this.applicationId}/stats`,
       stats,
@@ -38,3 +46,8 @@ export class KoreanBotsClient {
     return response
   }
 }
+
+export const koreanBots = new KoreanBots(
+  DISCORD_APPLICATION_ID,
+  KOREAN_DISCORD_LIST_TOKEN,
+)
