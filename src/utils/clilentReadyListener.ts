f5bc2b42de0d84794aa9947faecd89ede9ffb930
PMtHk <zooby88@gmail.com>
2024-12-10 16:19:39 +0900

    refactor: 매직넘버 제거 및 함수 분리

--- src/utils/clilentReadyListener.ts
+++ src/utils/clilentReadyListener.ts
@@ -3,23 +3,38 @@ import commands from '../commands'
 import { startWatch } from '../schedules/startWatch'
 import updateServerCount from './updateServerCount'
 
-export default async function clientReadyListener(client: Client<true>) {
+const UPDATE_SERVER_COUNT_INTERVAL = 1000 * 60 * 60 * 24 // 24시간
+
+function createSlashCommands(client: Client) {
+  return Promise.all(
+    commands.map((command) => client.application.commands.create(command)),
+  )
+}
+
+export default async function clientReadyListener(client: Client) {
+  console.log('Bot is starting')
+
   try {
-    if (!client.application) return
+    if (!client.application) {
+      console.error('client.application is not available')
+      return
+    }
 
-    console.log(process.env.NODE_ENV)
-    if (process.env.NODE_ENV === 'production')
+    if (process.env.NODE_ENV === 'production') {
+      const { cache } = client.guilds
+
       setInterval(
-        async () => await updateServerCount(client.guilds.cache.size),
-        1000 * 60 * 60 * 12, // 12 hour
+        async () => await updateServerCount(cache.size),
+        UPDATE_SERVER_COUNT_INTERVAL,
       )
+    }
 
-    // create slashCommands
-    for await (const command of commands)
-      await client.application.commands.create(command)
+    await createSlashCommands(client)
 
     startWatch(client)
   } catch (error) {
-    console.log('clientReadyListener error: ', error)
+    console.error('clientReadyListener error: ', error)
+  } finally {
+    console.log('Bot is ready')
   }
 }
