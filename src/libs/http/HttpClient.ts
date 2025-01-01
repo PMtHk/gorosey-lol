88bd6a54972999e17bfd28e86bbe412437be2d85
PMtHk <zooby88@gmail.com>
2025-01-01 17:43:32 +0900

    fix: HttpClient 의 timeout 으로 RateLimiter 가 동작하지 않는 문제 해결

--- src/libs/http/HttpClient.ts
+++ src/libs/http/HttpClient.ts
@@ -1,16 +1,16 @@
 import { HttpError } from './error'
 import { HttpClientConfig, RequestConfig } from './types'
 
-const DEFAULT_TIMEOUT = 10000
+// const DEFAULT_TIMEOUT = 10000
 
 export class HttpClient {
   constructor(private config: HttpClientConfig) {
-    this.config.timeout = config.timeout ?? DEFAULT_TIMEOUT
+    // this.config.timeout = config.timeout ?? DEFAULT_TIMEOUT
   }
 
   async fetch<T>(path: string, config?: RequestConfig) {
-    const controller = new AbortController()
-    const timeoutId = setTimeout(() => controller.abort(), this.config.timeout)
+    // const controller = new AbortController()
+    // const timeoutId = setTimeout(() => controller.abort(), this.config.timeout)
 
     try {
       const url = this.getURL(path)
@@ -21,7 +21,7 @@ export class HttpClient {
           ...this.config.headers,
           ...config?.headers,
         },
-        signal: controller.signal,
+        // signal: controller.signal,
         body: config?.body ? JSON.stringify(config.body) : undefined,
       })
 
@@ -36,7 +36,7 @@ export class HttpClient {
 
       throw new HttpError(error.message, 500)
     } finally {
-      clearTimeout(timeoutId)
+      // clearTimeout(timeoutId)
     }
   }
 
